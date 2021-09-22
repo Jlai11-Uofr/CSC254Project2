3f1546be70a9376b70876bfa6fb422789574bde3
jlai11-UofR <jlai11@u.rochester.edu>
2021-09-21 20:22:26 -0400

    New types check

--- scan.h
+++ scan.h
@@ -3,7 +3,7 @@
 */
 
 typedef enum {t_read, t_write, t_id, t_literal, t_gets, t_add,
-              t_sub, t_mul, t_div, t_lparen, t_rparen, t_eof} token;
+              t_sub, t_mul, t_div, t_lparen, t_rparen, t_eof,t_if, t_fi} token;
 
 #define MAX_TOKEN_LEN 200
 extern char token_image[MAX_TOKEN_LEN];
